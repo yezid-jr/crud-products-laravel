0e6f93721e323f0485df24398ad0fcbc1f3938e8
Yesid Castro <castrogil202@gmail.com>
2026-02-19 09:14:59 -0500

    update readme, and config in routes

--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::resource('products', ProductController::class);
 Route::middleware([
     'auth:sanctum',
     config('jetstream.auth_session'),
